d2f18b13bf54ad5b0a9d0ec50daee2d9ad8442a3
purnimaagrawal <purnima.agrawal1992@gmail.com>
2022-07-18 11:42:43 +0530

    Add API for client authentication

--- src/modules/authentication/clientAuthToken.controller.ts
+++ src/modules/authentication/clientAuthToken.controller.ts
@@ -1,15 +1,21 @@
-import { Controller,Post,Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { ClientAuthTokenService } from './clientAuthToken.service';
 
 @Controller('clientAuthToken')
 export class ClientAuthTokenController {
   constructor(private clientAuthToken: ClientAuthTokenService) {}
- 
+
   @Post()
-  async addClientAuthToken(
-    @Body() auth: any
-  ): Promise<any> {
-    return this.clientAuthToken.addKey(auth.client_id,auth.token);
+  async addClientAuthToken(@Body() auth: any): Promise<any> {
+    return this.clientAuthToken.addKey(auth.client_id, auth.token);
   }
 
+  @Post('authenticate-client')
+  async checkClientAuthentication(@Body() auth: any): Promise<any> {
+    return this.clientAuthToken.getKeySetting(
+      'api_key',
+      auth.api_key,
+      auth._clientId
+    );
+  }
 }
