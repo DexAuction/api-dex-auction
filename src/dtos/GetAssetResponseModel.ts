eb256929d8526fea1aad12cde4741eaa05e2ed49
Shivangi2022 <shivangi@drife.io>
2022-07-29 13:25:09 +0530

    eslint configuration

--- src/dtos/GetAssetResponseModel.ts
+++ src/dtos/GetAssetResponseModel.ts
@@ -1,16 +1,16 @@
 export interface GetAssetResponseModel {
-  assetContractAddress :string;
+  assetContractAddress: string;
   assetTokenId: string;
   mintedAt: Date;
   metadataURL: string;
-  external_url :string;
-  metadataJSON : Object;
+  external_url: string;
+  metadataJSON: Object;
   attributes: Object;
   background_color: string;
-  background_image : string;
+  background_image: string;
   owner: string;
   mintedBy: string;
-  name : string;
-  image : string,
-  description:string
+  name: string;
+  image: string;
+  description: string;
 }
